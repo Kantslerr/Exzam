9faa37cb763a87b05a66ca176498dffc939d9745
Kantslerr <KantslerR@rambler.ru>
2021-08-10 17:01:53 +0300

    4

--- pyls.py
+++ pyls.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3f = open("/home/kantsler/Desktop/project/index.html", 'w')
 import subprocess
+import os
 import string
 import re
 my_list = list()
-a = str(subprocess.check_output(["pwd"]))
-a = re.sub("[(|)|']" , "" ,a)
+a = os.popen('pwd', mode='r')
 z = "Information about directory:  ", a
 my_list.append(z)
 b =str(subprocess.check_output(["uname", "-o"]))
@@ -13,3 +13,4 @@ my_list.append(c)
 with open("/home/kantsler/Desktop/project/index.html", 'w') as f:
 	for x in my_list:
 		f.write(str(x) + "\n")
+
